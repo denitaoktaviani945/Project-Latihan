4d94dbd731fb740892684f850a633b9df467b921
unknown <denitaoktaviani45@gmail.com>
2023-07-30 23:22:08 +0700

    Belajar Mengenai Pengulangan While

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Pengulangan-While/while.js
@@ -0,0 +1,20 @@
+//infinity loop
+/*while(false){
+    console.log('hello world');
+}*/
+
+
+
+//di hentikan oleh user
+/*var ulang= true;
+while(ulang){
+    console.log('hello world');
+    ulang = confirm('lagi?');
+}*/
+
+// di hentikan oleh sistem cara 1
+var nilaiAwal = 1;
+while(nilaiAwal <=10){
+    console.log('hello world' + nilaiAwal + 'x');
+nilaiAwal++;
+}
