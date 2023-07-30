9aade9482897a0180fbc5307e07a45d8d041e0d6
unknown <denitaoktaviani45@gmail.com>
2023-07-30 14:48:30 +0700

    Membut PopUp menggunakan Alert, Prompt dan Confirm

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/PopUpBox/script.js
@@ -0,0 +1,17 @@
+//alert('selamat datng di web kami');
+
+//var nama =prompt('masukan nama:');
+//alert(nama);
+
+alert('selamat datang');
+var lagi=true;
+
+while (lagi) {
+    var  nama = prompt('masukan nama:');
+    alert ('hallo' + nama);
+
+    lagi = confirm ('coba lagi?');
+
+}
+
+alert('TERIMAKSIH');
