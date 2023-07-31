6f47b474ebbdc32a70a3e6d4f578964ed3b6f9bd
unknown <denitaoktaviani45@gmail.com>
2023-07-31 19:19:44 +0700

    MELATIH PENGULANGAN MENGGUNAKN FOR DAN WHILE

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Pengulangan-Tabel-Penelusuran/for.js
@@ -0,0 +1,17 @@
+/*for( var nilaiAwal=1 ; nilaiAwal <=10; nilaiAwal++;){
+    console.log('hello')
+}*/
+
+var jmlAngkot =20;
+var angkotBeroprasi = 13;
+var noAngkot =1;
+
+while ( noAngkot <=angkotBeroprasi){
+    console.log('angkot No. ' + noAngkot + ' beroprasi dengan baik');
+
+noAngkot++;
+}
+
+for( noAngkot = angkotBeroprasi +1; noAngkot <= jmlAngkot; noAngkot++ ){
+    console.log( ' angkot No. ' + noAngkot + ' sedang tidak beroprasi')
+}
