d283f8db8a840c7cad608b9afd16aaee7a87da74
unknown <denitaoktaviani45@gmail.com>
2023-08-02 15:28:41 +0700

    latihan membuat segitiga dari bintang menggunakan perulangan dan pengkondisian

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/LATIHAN/script.js
@@ -0,0 +1,42 @@
+// membuat bintang turun dan ke samping
+/*var d ='';
+
+for( var i=0; i < 10; i++){
+    for( var j=0; j < 5; j++){
+
+        d +='*';
+    }
+
+    d +="\n";
+}
+
+console.log(d)*/
+
+//mebuat bintang berurutan dari atas ke bawah
+/*var d ='';
+
+for( var i=0; i < 10; i++){
+    for( var j=0; j < 5; j++){
+
+        d +='*';
+    }
+
+    d +="\n";
+}
+
+console.log*/
+
+
+//membuat bintang berurutan dari bawah ke atas
+var d ='';
+
+for( var i=10; i > 0; i--){
+    for( var j=0; j < i; j++){
+
+        d +='*';
+    }
+
+    d +='\n';
+}
+
+console.log(d)
