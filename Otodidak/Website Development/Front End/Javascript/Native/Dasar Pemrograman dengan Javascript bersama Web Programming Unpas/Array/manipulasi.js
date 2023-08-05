f8e7084cd7ed2713d6a9aba5b170580d79d14d6d
unknown <denitaoktaviani45@gmail.com>
2023-08-05 23:32:42 +0700

    pembelajaran tentang dasar dari pembuatan sebuah array

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Array/manipulasi.js
@@ -0,0 +1,23 @@
+// // manipulasi array
+
+// // menanmbah isi array
+// var arr = [];
+// arr [0]= "denita";
+// arr [1]= "oktaviani";
+// arr[2] = "reza";
+// arr [3]= "fauzan";
+// arr [4]= " adhima";
+
+// console.log (arr);
+
+// // menghapus array
+// var arr = ["denita","oktaviani","reza","fauzan","adhima"];
+// arr[4] = undefined;
+// console.log(arr);
+
+// menampilkan isi array
+var arr = ["denita","oktaviani","reza","fauzan"];
+
+for( var i = 0; i < 10; i++){
+    console.log ('mahasisa ke-' + (i+1) + ':' + arr[i]);
+}
