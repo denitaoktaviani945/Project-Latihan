d0ffdf34f73000f7c777787ef92c632a2e4319d7
unknown <denitaoktaviani45@gmail.com>
2023-08-10 23:54:49 +0700

    final project dari pembelajaran dasar javaScript membuat program ntuk angkot

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/FINAL-PROJECT/script.js
@@ -0,0 +1,37 @@
+ // mmembuat object angkot 
+ function Angkot (sopir , trayek , penumpang ,kas) {
+    this.sopir = sopir;
+    this.trayek = trayek ;
+    this.penumpang = penumpang;
+    this.kas = kas ;
+
+
+    this.penumpangNaik = function (namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun =function(namaPenumpang, bayar){
+
+        if (this.penumpang.length ===0){
+            alert ('angkot masih kosong');
+            return false ;
+        }
+
+        for ( var i = 0 ; i < this.penumpang.length ; i ++){
+            if (this.penumpang [i]  == namaPenumpang){
+                this.penumpang [i] = undefined ;
+                this.kas += bayar;
+
+                return this.penumpang;
+            }
+        }
+
+
+    }
+
+
+ }
+
+var angkot1 =new Angkot ('usep',['soreang,bandung'], [],0);
+var angkot2 =new Angkot ('arif',['soreang,ciwidey'], [],0);
